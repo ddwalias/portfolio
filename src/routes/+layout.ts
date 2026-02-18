c8b4b7965df0f8e26ec1153af0e7ae789278e9c9
Duy Dang <55247256+ddwalias@users.noreply.github.com>
2026-02-18 16:27:36 +0700

    Add vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,6 @@
 export const prerender = true;
 export const trailingSlash = 'always';
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
